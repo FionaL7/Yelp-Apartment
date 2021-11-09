a4ea7bd17cdb4018e0e068a1ea9656e5104ff19e
Fiona Lazarus <fionalazarus7@gmail.com>
2021-11-09 13:27:30 +0530

    mapbox access token to be fixed

--- public/javascripts/showMapPage.js
+++ public/javascripts/showMapPage.js
@@ -1,16 +1,16 @@
+
 mapboxgl.accessToken = mapToken;
-  const map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
   center: apartment.geometry.coordinates, // starting position [lng, lat]
   zoom: 8 // starting zoom
-  });
+});
 
-  new mapboxgl.Marker()
-.setLngLat(apartment.geometry.coordinates)
-.setPopup(
+new mapboxgl.Marker()
+  .setLngLat(apartment.geometry.coordinates)
+  .setPopup(
     new mapboxgl.Popup({ offset: 25 }) // add popups
       .setHTML(`<h3>${apartment.title}</h3>`)
   )
-.addTo(map);
- 
+  .addTo(map);
